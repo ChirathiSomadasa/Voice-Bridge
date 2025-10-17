af2c7f8dd799f27af9e5c48c7a24bfb8688e7f58
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-10-17 21:47:37 +0530

    Implement Create Account Page UI

--- app/src/main/java/com/chirathi/voicebridge/LoginActivity.kt
+++ app/src/main/java/com/chirathi/voicebridge/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.chirathi.voicebridge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val createAccount = findViewById<Button>(R.id.btn_create_account)
+        createAccount.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
