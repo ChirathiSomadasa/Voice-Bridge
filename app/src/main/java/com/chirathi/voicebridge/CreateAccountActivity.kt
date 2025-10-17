8c7068e738f5d7f86b6970b053cdd7f1ed8bd6fb
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-10-17 23:09:25 +0530

    Implement Teacher Registration Page UI

--- app/src/main/java/com/chirathi/voicebridge/CreateAccountActivity.kt
+++ app/src/main/java/com/chirathi/voicebridge/CreateAccountActivity.kt
@@ -16,5 +16,22 @@ class CreateAccountActivity : AppCompatActivity() {
             val intent = Intent(this, ChildRegistrationActivity::class.java)
             startActivity(intent)
         }
+
+        val teacherBtn = findViewById<LinearLayout>(R.id.teacher_layout)
+        teacherBtn.setOnClickListener {
+            val intent = Intent(this, TeacherRegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        val child = findViewById<Button>(R.id.child_btn)
+        child.setOnClickListener {
+            val intent = Intent(this, ChildRegistrationActivity::class.java)
+            startActivity(intent)
+        }
+        val teacher = findViewById<Button>(R.id.teacher_btn)
+        teacher.setOnClickListener {
+            val intent = Intent(this, TeacherRegistrationActivity::class.java)//
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/chirathi/voicebridge/TeacherRegistrationActivity.kt
@@ -0,0 +1,32 @@
+package com.chirathi.voicebridge
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class TeacherRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_registration)
+
+        val signupBtn = findViewById<Button>(R.id.signup_btn)
+        signupBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val loginLink = findViewById<TextView>(R.id.link_signin)
+        loginLink.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val backBtn = findViewById<ImageView>(R.id.back)
+        backBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
