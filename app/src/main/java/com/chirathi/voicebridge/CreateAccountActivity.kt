2cfbad34c7d486d600d73c851df4695592b6580b
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-10-17 22:40:32 +0530

    Implement Child Registration Page UI

--- app/src/main/java/com/chirathi/voicebridge/CreateAccountActivity.kt
+++ app/src/main/java/com/chirathi/voicebridge/CreateAccountActivity.kt
@@ -1,11 +1,20 @@
 package com.chirathi.voicebridge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        val childBtn = findViewById<LinearLayout>(R.id.child_layout)
+        childBtn.setOnClickListener {
+            val intent = Intent(this, ChildRegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
