b58a09b6c76c3b5dcf95d7ef93ef89d24c4624b6
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-10-21 02:03:52 +0530

    create symbol dashboard

--- app/src/main/java/com/chirathi/voicebridge/HomeFragment.kt
+++ app/src/main/java/com/chirathi/voicebridge/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.chirathi.voicebridge
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -22,6 +23,13 @@ class HomeFragment : Fragment() {
             val intent = Intent(context, SpeechPracticeActivity::class.java)
             startActivity(intent)
         }
+
+        val symbolCommunication = view.findViewById<ConstraintLayout>(R.id.symbol_communication)
+        symbolCommunication.setOnClickListener {
+            val intent = Intent(context, SymbolCommunicationActivity::class.java)
+            startActivity(intent)
+        }
+
         return view
     }
 }
