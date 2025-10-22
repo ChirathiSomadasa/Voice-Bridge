8b3cc8345930c7f9f27bb9d018a805d0ab9a7596
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-10-22 22:05:57 +0530

    update quick words charts

--- app/src/main/java/com/chirathi/voicebridge/QuickWordsChartsActivity.kt
+++ app/src/main/java/com/chirathi/voicebridge/QuickWordsChartsActivity.kt
@@ -2,24 +2,26 @@ package com.chirathi.voicebridge
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class QuickWordsActivity : AppCompatActivity()  {
+class QuickWordsChartsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quick_words)
+        setContentView(R.layout.activity_quick_words_chart)
 
         val backBtn = findViewById<ImageView>(R.id.back)
         backBtn.setOnClickListener {
             finish()
         }
-        val goBtn = findViewById<Button>(R.id.btn_Go_quick)
-        goBtn.setOnClickListener {
-            val intent = Intent(this, QuickWordsChartsActivity::class.java)
+
+        val playIcon = findViewById<ImageView>(R.id.imgPlay)
+        val stopIcon = findViewById<ImageView>(R.id.imgStop)
+
+        playIcon.setOnClickListener {
+            val intent = Intent(this, PhraseActivity::class.java)
             startActivity(intent)
         }
-
     }
+
 }
