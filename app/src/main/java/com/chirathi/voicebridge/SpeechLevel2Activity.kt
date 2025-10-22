1194cfed07dea72bcedd1950bcbb2717a92b9e5a
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-10-23 01:08:59 +0530

    Create speech-level2 activity page UI

--- app/src/main/java/com/chirathi/voicebridge/SpeechLevel2Activity.kt
+++ app/src/main/java/com/chirathi/voicebridge/SpeechLevel2Activity.kt
@@ -1,13 +1,21 @@
 package com.chirathi.voicebridge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class SpeechLevel2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_speech_level2)
+
+        val speakBtn = findViewById<Button>(R.id.btn_level2)
+        speakBtn.setOnClickListener {
+            val intent = Intent(this, SpeechLevel2TaskActivity::class.java)
+            startActivity(intent)
+        }
 
         val backBtn = findViewById<ImageView>(R.id.back)
         backBtn.setOnClickListener {
