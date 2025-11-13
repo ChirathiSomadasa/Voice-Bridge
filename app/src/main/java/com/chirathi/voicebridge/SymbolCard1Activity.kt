a9c358886b312044f5e140350c31d022da2d3646
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-11-13 10:33:47 +0530

    update to symbol chart

--- app/src/main/java/com/chirathi/voicebridge/SymbolCard1Activity.kt
+++ app/src/main/java/com/chirathi/voicebridge/SymbolCard1Activity.kt
@@ -6,21 +6,21 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class SymbolChartActivity : AppCompatActivity() {
+class SymbolCard1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_symbol_chart)
+        setContentView(R.layout.activity_symbol_card1)
 
         val backBtn = findViewById<ImageView>(R.id.back)
         backBtn.setOnClickListener {
             finish()
         }
 
-        val goBtn = findViewById<Button>(R.id.btn_Go_symbol)
-        goBtn.setOnClickListener {
-            val intent = Intent(this, QuickWordsChartsActivity::class.java)
-            startActivity(intent)
-        }
+//        val goBtn = findViewById<Button>(R.id.btn_Go_symbol)
+//        goBtn.setOnClickListener {
+//            val intent = Intent(this, QuickWordsChartsActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
