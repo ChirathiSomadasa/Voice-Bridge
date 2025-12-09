d34bd026a6aabd8a1282bd57fad9dbc6815c136c
Hirushini-cygn <HirushiniRashmika.C@gmail.com>
2025-12-09 19:52:13 +0530

    added skip button and it logic

--- app/src/main/java/com/chirathi/voicebridge/MoodMatchSevenDownActivity.kt
+++ app/src/main/java/com/chirathi/voicebridge/MoodMatchSevenDownActivity.kt
@@ -1,4 +1,23 @@
 package com.chirathi.voicebridge
 
-class MoodMatchSevenDownActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class MoodMatchSevenDownActivity  : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mood_match_seven_down)
+
+        val rootLayout = findViewById<View>(android.R.id.content)
+        rootLayout.setOnClickListener {
+
+            val intent = Intent(this, GameDashboardActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish()
+        }
+    }
 }
