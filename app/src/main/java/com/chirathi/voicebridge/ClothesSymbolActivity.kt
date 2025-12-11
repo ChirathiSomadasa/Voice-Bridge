013807fd205c597225c0dce7f38ccb5ca6ab1ba3
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-12-11 20:04:24 +0530

    create clothes symbol screen

---
+++ app/src/main/java/com/chirathi/voicebridge/ClothesSymbolActivity.kt
@@ -0,0 +1,21 @@
+package com.chirathi.voicebridge
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ClothesSymbolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_clothes_symbol)
+
+        val backBtn = findViewById<ImageView>(R.id.back)
+        backBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
