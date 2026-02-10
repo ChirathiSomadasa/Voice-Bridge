95d3c112bfe50b448f43ee11abc9630ff5891db0
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2026-02-10 12:24:00 +0530

    implement to generate sentence for detect objects

---
+++ app/src/main/java/com/chirathi/voicebridge/ApiService.kt
@@ -0,0 +1,17 @@
+package com.chirathi.voicebridge
+
+import okhttp3.MultipartBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+data class CaptionResponse(
+    val caption: String
+)
+
+interface ApiService {
+    @Multipart
+    @POST("caption")
+    fun uploadImage(@Part image: MultipartBody.Part): Call<CaptionResponse>
+}
---
+++ app/src/main/java/com/chirathi/voicebridge/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.chirathi.voicebridge
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://192.168.244.45:5000/"
+    val instance: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
